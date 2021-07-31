dabc1a3d476488ef3b902fdb37b214bf6703697f
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-07-31 12:15:10 +0200

    iframe done

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,18 @@ import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  // const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
   useEffect(() => {
@@ -23,17 +25,48 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcDoc = html
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
     });
 
     // console.log(result);
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text,'*')
+
+  
   };
+
+const html = `
+  <html>
+  <head></head>
+  <body>
+  <div id="root"></div>
+  <script>
+  window.addEventListener('message',(event)=>{
+    try {
+      eval(event.data)
+    } catch(err) {
+      const root = document.querySelector('#root');
+      root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>'+ err +'</div>'
+      throw err
+    }
+    
+  },false)
+  </script
+  </body>
+  </html>
+`;
+;
 
   return (
     <div>
@@ -44,9 +77,10 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe sandbox="allow-scripts" srcDoc={html} ref={iframe} title="code" />
     </div>
   );
 };
+
 
 ReactDOM.render(<App />, document.querySelector("#root"));
