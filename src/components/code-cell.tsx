4303f03593432a7682f23267fcbd8f4148c2b213
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-08-05 12:58:27 +0200

    Code fixup

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import CodeEditor from "./code-editor";
+import Preview from "./Preview";
+import bundle from "../bundler";
+import Resizable from "./resizable";
+
+const CodeCell = () => {
+  const [code, setCode] = useState("");
+  const [input, setInput] = useState("");
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <Resizable direction="vertical">
+      <div>
+        <CodeEditor
+          initialValue="//write your code here"
+          onChange={(value) => setInput(value)}
+        />
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
