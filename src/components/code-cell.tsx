fb22b61765f3bf2cb0e7e7babdfeea68bda201e6
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-08-05 15:07:08 +0200

    Resizing the editor : done

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./Preview";
 import bundle from "../bundler";
@@ -8,21 +8,26 @@ const CodeCell = () => {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+   const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return()=>{
+      clearTimeout(timer);
+    }
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
-      <div>
-        <CodeEditor
-          initialValue="//write your code here"
-          onChange={(value) => setInput(value)}
-        />
-        <div>
-          <button onClick={onClick}>Submit</button>
-        </div>
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="//Write your code here"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
